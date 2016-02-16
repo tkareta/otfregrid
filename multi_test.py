00a001ac13bc6f57c5d62a7d708b951d40033599
Theodore Kareta <tkareta@umass.edu>
2016-02-16 08:17:19 -0500

    double checking 'multiprocessing.Array' is truly a numpy array

--- multi_test.py
+++ multi_test.py
@@ -11,6 +11,7 @@ class gridholder(object):
             grid = multiprocessing.Array(ctypes.c_double, 10*10)
             grid = numpy.ctypeslib.as_array(grid.get_obj())
             grid = grid.reshape(10,10)
+            print type(grid)
             self.grid = grid
             print grid
 
@@ -24,7 +25,7 @@ def func(grid, n):
         c1 = numpy.random.randint(0, size[0])
         c2 = numpy.random.randint(0, size[1])
         grid[c1,c2] += 1.0
-        print "wrote a point"
+        #print "wrote a point"
 
 
 if __name__=="__main__":
@@ -39,4 +40,5 @@ if __name__=="__main__":
     plt.colorbar()
     plt.show()
     print g.grid.sum()
+    print type(g.grid)
     
