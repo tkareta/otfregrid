11a9e80c7a5aac65558657203fa146b7ee93be27
Theodore Kareta <tkareta@umass.edu>
2016-06-02 11:14:41 -0400

    changes from approx. 1 month ago -- probably minor, unsure

--- gridmaker_1c.py
+++ gridmaker_1c.py
@@ -63,6 +63,7 @@ def convolve_wrapper_cy(filename, biased, sigmaweight,tsysweight,RMAX, crval2, c
 
 
 if __name__ == "__main__":
-    files = ['35065305.nc','35065304.nc','35065306.nc','35065300.nc']#,'35065301.nc','35065302.nc','35065303.nc','35065307.nc','35065308.nc','35065309.nc']
+    files = ['35065305.nc','35065304.nc','35065306.nc','35065300.nc','35065301.nc','35065302.nc','35065303.nc','35065307.nc','35065308.nc','35065309.nc']
     grid = gridmaker_1c(525.0,535.0,645.0,655.0, files)
+    
     
